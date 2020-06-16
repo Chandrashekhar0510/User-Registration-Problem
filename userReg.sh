95bacced5a740f9d597979f77f077ba3f732fe79
Chandrashekhar0510 <rathodsahul.gmail.com>
2020-06-16 19:13:09 +0530

    UC-1 : first name validation

--- userReg.sh
+++ userReg.sh
@@ -2,4 +2,18 @@
 
 shopt -s extglob
 
+echo ""
 echo "Welcome to User Registration System"
+echo "======================================"
+echo ""
+
+read -p "Enter first name : " fname
+
+fnamePattern="^[[:upper:]]+([a-z]{2,})"
+
+if [[ $fname =~ $fnamePattern ]]
+then
+	echo "Valid name"
+else
+	echo "Invalid name"
+fi
