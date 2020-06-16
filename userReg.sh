0fe044997e7eadd6c58ac6f6868e9090ecba416b
Chandrashekhar0510 <rathodsahul.gmail.com>
2020-06-16 19:24:23 +0530

    UC-2 : validate last name

--- userReg.sh
+++ userReg.sh
@@ -7,13 +7,32 @@ echo "Welcome to User Registration System"
 echo "======================================"
 echo ""
 
+
+function validateFirstName()
+{
+	fnamePattern="^[[:upper:]]+([a-z]{2,})"
+
+	if [[ $fname =~ $fnamePattern ]]
+	then
+		echo "Valid name"
+	else
+		echo "Invalid name"
+	fi
+}
 read -p "Enter first name : " fname
+validateFirstName $fname
 
-fnamePattern="^[[:upper:]]+([a-z]{2,})"
 
-if [[ $fname =~ $fnamePattern ]]
-then
-	echo "Valid name"
-else
-	echo "Invalid name"
-fi
+function validateLastName()
+{
+        lnamePattern="^[[:upper:]]+([a-z]{2,})"
+
+        if [[ $lname =~ $lnamePattern ]]
+        then
+                echo "Valid name"
+        else
+                echo "Invalid name"
+        fi
+}
+read -p "Enter last name : " lname
+validateLastName $lname
